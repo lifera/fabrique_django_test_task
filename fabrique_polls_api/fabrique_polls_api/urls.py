fbecb229a5e035c272828d04fffcfb7eb8a023b6
lifera <lifeera@gmail.com>
2021-02-12 17:20:29 +0300

    post -> patch

--- fabrique_polls_api/fabrique_polls_api/urls.py
+++ fabrique_polls_api/fabrique_polls_api/urls.py
@@ -27,9 +27,9 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('polls/<int:pk>/questions/', QuestionList.as_view({'post': 'create', 'get': 'list'}), name='question_list'),
     path('polls/<int:pk>/questions/<int:question_pk>/', QuestionList.as_view(
-        {'get': 'retrieve', 'post': 'update', 'delete': 'destroy'}), name='question_change'),
+        {'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy'}), name='question_change'),
     path('polls/<int:pk>/questions/<int:question_pk>/answers/', AnswerCreate.as_view(), name='answer_create'),
-    path('answers/<int:pk>', AnswerList.as_view(), name='answer_list'),
+    path('answers/<int:pk>/', AnswerList.as_view(), name='answer_list'),
     path(r'docs/', include_docs_urls(title='Polls API')),
 ]
 
